cd44564774680626aa846704f84da0628b622082
Jun-Seok-K <79389996+Jun-Seok-K@users.noreply.github.com>
2023-08-17 10:01:16 +0900

    gitignore 수정 controller 추가 service 추가 저장, 수정, 조회 추가 저장, 수정, 조회 테스트 추가 BaseTimeEntity 추가 lombok, h2, JPA 의존성 추가

---
+++ src/main/java/com/webservice/springboot/service/posts/PostsService.java
@@ -0,0 +1,39 @@
+package com.webservice.springboot.service.posts;
+
+
+import com.webservice.springboot.domain.posts.Posts;
+import com.webservice.springboot.domain.posts.PostsRepository;
+import com.webservice.springboot.web.dto.PostsResponseDto;
+import com.webservice.springboot.web.dto.PostsSaveRequestDto;
+import com.webservice.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+    
+    // PostsApiControllerTest 참고(책 p.114)
+}
