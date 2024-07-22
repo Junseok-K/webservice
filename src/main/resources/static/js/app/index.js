33e61e86e03971e7dc6d8671d6a49c868e83fd8a
Jun-Seok-K <79389996+Jun-Seok-K@users.noreply.github.com>
2024-07-22 22:51:40 +0900

    index.js 오타수정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -24,7 +24,7 @@ var main = {
             type: 'POST',
             url: '/api/v1/posts',
             dataType: 'json',
-            contentType:'application/json; charset=utf-8',
+            contentType: 'application/json; charset-utf-8',
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
