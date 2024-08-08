a7694778a71cc719944ed833259ce6979bdb2eb5
Jun-Seok-K <79389996+Jun-Seok-K@users.noreply.github.com>
2024-08-08 21:07:48 +0900

    OAUTH 연동 시 권한 USER 세팅

--- src/main/java/com/webservice/springboot/Application.java
+++ src/main/java/com/webservice/springboot/Application.java
@@ -2,12 +2,19 @@ package com.webservice.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // @EnableJpaAuditing // JPA Auditing 활성화 -> JpaConfig로 이동
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        System.out.println("■■■■■■■■■■ Application Start ■■■■■■■■■■");
+        System.out.println("http://localhost:8080/");
+        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+
+        System.out.println("■■■■■■■■■■ Database Edit ■■■■■■■■■■");
+        System.out.println("http://localhost:8080/h2-console/");
+        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
     }
 }
